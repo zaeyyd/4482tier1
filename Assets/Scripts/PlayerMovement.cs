cd9c62e9422767bf3d085400c8c6af6b8434aeff
Zayyed Mansoor <zayyed.not@gmail.com>
2022-09-30 15:09:34 -0400

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // CALLED BEFORE FIRST FRAME UPDATE
+    void Start()
+    {
+        
+    }
+
+    // CALLED ONCE PER FRAME
+    void Update()
+    {
+        float horizontal = Input.GetAxis ("Horizontal");
+    }
+
+
+
+}
